985d622d8a90d2561fae2ad366fa6d0d03ea5d1d
r4nd0lph <r11345roma@yandex.ru>
2024-10-15 14:55:07 +0300

    init repo

---
+++ tuner.py
@@ -0,0 +1,11 @@
+# CHROMATIC TUNER – tune to every note in the chromatic scale
+# https://github.com/r4nd0lph-c/tuner
+
+
+class Tuner:
+    def __init__(self) -> None:
+        pass
+
+
+if __name__ == "__main__":
+    pass
